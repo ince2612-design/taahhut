8effa105bb215111be44bfe4244d905b7bacb4b2
ince2612-design <ince2612@gmail.com>
2026-07-16 14:21:43 +0300

    Update noter.py
    DFGFG

--- noter.py
+++ noter.py
@@ -52,8 +52,6 @@ if st.button("BELGE OLUŞTUR"):
     
     pdf = FPDF(orientation='P', unit='mm', format='A4')
     pdf.add_page()
-    pdf = FPDF(orientation='P', unit='mm', format='A4')
-    pdf.add_page()
     
     # fpdf2'nin kendi içindeki gömülü fontu, unicode desteği ile kullanıyoruz
     pdf.set_font("helvetica", size=14) 
@@ -61,7 +59,6 @@ if st.button("BELGE OLUŞTUR"):
     # Eğer bu hata verirse, lütfen "TAAHHÜTNAME" gibi özel karakter içeren 
     # kelimeleri şu şekilde yazmayı deneyin:
     # "TAAHHÜTNAME".encode('latin-1', 'replace').decode('latin-1')
-    
     # Font değişikliği yapıldı: DejaVuSans.ttf yerine "helvetica" kullanılıyor
     pdf.set_font("helvetica", size=14)
     pdf.cell(0, 8, "TAAHHÜTNAME", ln=True, align='C')
